5d41a0a088cba9165af291ae597cb708e40419c7
Sahiljangra115 <sahil115jangid@gmail.com>
2025-07-20 02:12:23 +0530

    Cleanup: remove unwanted files

---
+++ LLM's/llm_demo.py
@@ -0,0 +1,21 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+import os
+
+# Load API key from .env
+load_dotenv()
+api_key = os.getenv("OPENROUTER_API_KEY")
+
+# Set the base URL for OpenRouter
+openai_base_url = "https://openrouter.ai/api/v1"
+
+# Initialize the chat mmodel = ""odel
+llm = ChatOpenAI(
+    openai_api_key=api_key,
+    openai_api_base=openai_base_url,
+    model="moonshotai/kimi-k2:free",
+)
+
+# Try a simple message
+response = llm.invoke("What is the capital of india")
+print(response.content)
